52866765ad1dd8697333a52e9c2f9e7852cac603
nthmost <naomi@nthmost.com>
2019-08-10 13:07:17 -0700

    set version 0.0.5 for py3k updates

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class build_ext(_build_ext):
 
 setup (
     name = 'metavariant',
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'a lexical manipulation toolkit for genetic variant descriptors (hgvs, etc)',
     author = 'Naomi Most',
     maintainer = 'Naomi Most',
